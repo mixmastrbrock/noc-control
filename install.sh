ff45975ac277c9031944d893bd01db323a5aae8f
mixmastrbrock <66269339+mixmastrbrock@users.noreply.github.com>
2020-06-01 12:10:55 -0500

    install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash\
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install wakeonlan
+brew install wget
+wget "https://github.com/mixmastrbrock/noc-control/raw/master/noc-control.zip"
+unzip noc-control.zip
+chmod -x ./noc-control/pod.sh
+chmod -x ./noc-control/pod-off.sh
+chmod -x ./noc-control/pod-a.sh
+chmod -x ./noc-control/pod-b.sh
+chmod -x ./noc-control/pod-c.sh
+chmod -x ./noc-control/pod-d.sh
+chmod -x ./noc-control/pod-a-off.sh
+chmod -x ./noc-control/pod-b-off.sh
+chmod -x ./noc-control/pod-c-off.sh
+chmod -x ./noc-control/pod-d-off.sh
+rm noc-control.zip
+exit
